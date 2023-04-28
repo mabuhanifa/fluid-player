e8c1ef304211e52c422aee8c913b7e9fa35aedb5
shourovahmd <moh.abuhanifa@gmail.com>
2023-04-28 20:12:20 +0600

    classes new

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const input = document.getElementById("input");
 const btn = document.getElementById("btn");
+const src = document.getElementById("source");
+const video = document.getElementById("video-id");
+
+video.style.display = "none";
 
 btn.addEventListener("click", () => {
-  console.log(input.value);
+  src.src = input.value;
+  video.style.display = "block";
+  console.log(src.src, "input",input.value);
 });
